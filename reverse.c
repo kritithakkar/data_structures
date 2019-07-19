98750ca7dbff9818dacce4416c21ab9dd7ea1d7d
kritithakkar <kritithakker@gmail.com>
2019-07-19 15:19:59 +0530

    reverse

---
+++ reverse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,char* argv[])
+{
+int i,arr[argc-1];
+for(i=1;i<argc;i++)
+{
+arr[i]=atoi(argv[i]);
+}
+int *ptr=&arr[argc-1];
+for(i=argc-1;i>0;i--)
+{
+printf("%d ",*ptr);
+ptr--;
+}
+}
