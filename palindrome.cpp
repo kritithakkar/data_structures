28dc4cefb8abe0f2812b2e88d2adf9734726f9a2
kritithakkar <kritithakker@gmail.com>
2019-08-01 23:53:39 +0530

    palindrome

---
+++ palindrome.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(int argc,char *argv[])
+{
+string s,m;
+s=argv[1];
+int d=s.size();
+for(int i=d;i>=0;i--)
+{
+ m=s;
+}
+if(s==m)
+{
+cout<<"palindrome";
+}
+else
+{
+cout<<"not palindrome";
+}
+return 0;
+}
